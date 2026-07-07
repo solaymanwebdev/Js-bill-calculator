2154c4dd72721420c8f035c9e2f0fbb76911852b
Solayman Shahid <solaymanshahid67@gmail.com>
2026-07-07 14:24:44 +0600

    Add user input with prompt() in bill calculator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let itemPrice = 100;
-let quantity = 3;
-let dicountPercent = 20;
+let itemPrice = Number(prompt(`How much item ?`)) ;
+let quantity = Number(prompt(`How many quantity ?`)) ;
+let dicountPercent = Number(prompt(`How many discount ?`)) ;
 
 let subTotal = itemPrice * quantity;
 let off = (subTotal * dicountPercent) /100;
@@ -10,3 +10,15 @@ document.getElementById('productAmount').textContent = `Item Price : ${itemPrice
 document.getElementById('subTotal').textContent = `Sub Total : ${subTotal}`;
 document.getElementById('discount').textContent = `Discount Price : ${off}`;
 document.getElementById('finalTotal').textContent = `Total Cash : ${finalPay}`
+
+
+//let learners = 30;
+
+//learners = learners +1 ;
+//learners = learners -1;
+//learners = learners *2;
+//learners = learners /2;
+//learners = learners **2;
+//learners = learners % 2;
+
+//console.log(learners);
